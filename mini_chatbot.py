9b77fbf147f64d3a538c11e99a7f8889f6c46be2
C1ey <105993415+C1ey@users.noreply.github.com>
2024-01-28 12:47:49 -0800

    Dictionary Implementation

--- mini_chatbot.py
+++ mini_chatbot.py
@@ -5,6 +5,26 @@ import os
 import socket
 import time
 
+
+waiting = 2
+time.sleep(waiting)
+test = ["*" for i in range(3)]
+
 def chatbot():
     Password = "Password1"
     Greeting= input("Password: ")
+    if Greeting != Password:
+        print("Incorrect Password, Access Denied!")
+        exit()
+    else:
+        if Greeting == Password:
+            print(test)
+
+#Define a dictionary of responses
+            responses = {
+                "default":"I am not sure how to respond to that",
+                "hello":"Hello, how may I assist you?", 
+                "What are you able to assist me with":"I can help you with general knowledge inquiries", 
+                "goodbye":"Goodbye!"
+            }
+            
